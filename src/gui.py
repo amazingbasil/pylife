8abd7c21ae05d2b9f07533d1c98efe1f4a94f80d
zuber <amazingbasil@gmail.com>
2015-06-06 00:20:37 +0600

    init commit

---
+++ src/gui.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+from processor import *
+
+
+class LifeGUI:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.prev_x = self.prev_y = None
+        self.root = Tk()
+        self.field = init_field(height=self.height, width=self.width)
+        self.canvas = Canvas(self.root, width=10 * len(self.field[0]), height=10 * len(self.field))
+        self.clear()
+        self.next_button = Button(self.root, bg="green", text="step", command=self.step)
+        self.next_button.pack()
+        self.clear_button = Button(self.root, bg="red", text="clean", command=self.clear)
+        self.clear_button.pack()
+        self.canvas.bind("<Button-1>", self.switch_cell)
+        self.canvas.bind("<B1-Motion>", self.switch_cell)
+        self.canvas.bind("<ButtonRelease-1>", self.forget)
+        self.canvas.pack()
+        self.root.mainloop()
+
+    def switch_cell(self, event):
+        x = event.x / 10
+        y = event.y / 10
+        if (self.prev_x, self.prev_y) != (x, y):
+            self.field[y][x] = not self.field[y][x]
+            self.redraw()
+        self.prev_x, self.prev_y = x, y
+
+    def forget(self, event):
+        self.prev_y = self.prev_x = None
+
+    def step(self):
+        self.field = next_step(self.field)
+        self.redraw()
+
+    def clear(self):
+        self.field = init_field(height=self.height, width=self.width)
+        self.redraw()
+
+    def redraw(self):
+        self.canvas.delete("all")
+        for i in range(len(self.field)):
+            for j in range(len(self.field[0])):
+                self.canvas.create_rectangle(j * 10, i * 10, (j + 1) * 10 - 2, (i + 1) * 10 - 2, fill="blue" if self.field[i][j] else "orange")
+
+
+
+def main():
+
+    LifeGUI(40, 40)
+
+if __name__ == '__main__':
+    main()
---
+++ src/processor.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+def init_field(height=10, width=10, alives=()):
+    fld = [[False] * width for _ in range(height)]
+    for alive in alives:
+        fld[alive[0]][alive[1]] = True
+    return fld
+
+def alive_neighbours(field, x, y):
+    result = []
+    for i in range(-int(y != 0), int(y != len(field[0]) - 1) + 1):
+        for j in range(-int(x != 0), int(x != len(field) - 1) + 1):
+            if (i or j) and field[x + j][y + i]:
+                result.append((x + j, y + i))
+    return result
+
+def next_step(fld):
+    result = [row[:] for row in fld]
+    for i in range(len(fld)):
+        for j in range(len(fld[1])):
+            if fld[i][j] and len(alive_neighbours(fld, i, j)) not in (2, 3):
+                result[i][j] = False
+            elif len(alive_neighbours(fld, i, j)) == 3:
+                result[i][j] = True
+    return result
+
+
+
+
+
+
